e7c92ee9e7d4f374ccaeda32e6a11321ccd6e590
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-10-20 20:09:05 +0900

    Initial Commit

---
+++ lit_cpbl_b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lit_cpbl_b
+//
+//  Created by 小神寛晴 on 2018/10/20.
+//  Copyright © 2018年 Kansei Kogami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
