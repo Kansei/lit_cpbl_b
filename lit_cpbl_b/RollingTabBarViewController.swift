b01da77e3519917641ffee4452d2dc062b0895a8
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-10-21 14:07:46 +0900

    modify: tabbar item

---
+++ lit_cpbl_b/RollingTabBarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RollingTabBarViewController.swift
+//  lit_cpbl_b
+//
+//  Created by 小神寛晴 on 2018/10/21.
+//  Copyright © 2018年 Kansei Kogami. All rights reserved.
+//
+
+import UIKit
+
+class RollingTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITabBar.appearance().tintColor = UIColor.white
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
